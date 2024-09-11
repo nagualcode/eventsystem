0bd4d112cbf850076d230c6cca81d0abd021a372
Fred <nagual@nagual.local>
2024-09-10 22:15:45 -0300

    auto

--- src/main/java/br/nagualcode/eventsystem/models/Order.java
+++ src/main/java/br/nagualcode/eventsystem/models/Order.java
@@ -1,9 +1,12 @@
 package br.nagualcode.eventsystem.models;
 
 import jakarta.persistence.Entity;
+
 import jakarta.persistence.Id;
 
+import jakarta.persistence.Table;
 @Entity
+@Table(name = "orders") 
 public class Order {
 
     @Id
